ed1b9fe7308cc35a4648a3b4181a82737e1f55a9
EdielRodrigues <diel_zi_nho25@hotmail.com>
2026-07-21 19:26:22 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,14 @@
 window.APP_CONFIG = {
   demoMode: true,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+  apiKey: "AIzaSyCdI11L3Y2GFCy4XzbpSwYGchEtFBzj6Sw",
+  authDomain: "ferramentas-projeto.firebaseapp.com",
+  databaseURL: "https://ferramentas-projeto.firebaseio.com",
+  projectId: "ferramentas-projeto",
+  storageBucket: "ferramentas-projeto.appspot.com",
+  messagingSenderId: "877191590019",
+  appId: "1:877191590019:web:e060a55b704e510d53abb6",
+  measurementId: "G-23LG8QCPP8"
   },
   functionsBaseUrl: "",
   currency: "BRL",
