f93793b933c59522f900220cbb43eee2fd472f58
EdielRodrigues <diel_zi_nho25@hotmail.com>
2026-07-23 15:21:29 -0300

    Add files via upload

---
+++ guia-iniciante/videos.js
@@ -0,0 +1,8 @@
+window.BEGINNER_VIDEOS = [
+  { id: "primeiros_passos", title: "Primeiros passos", url: "guia-iniciante/videos/01-primeiros-passos.mp4" },
+  { id: "aquecimento", title: "Aquecimento rápido", url: "guia-iniciante/videos/02-aquecimento.mp4" },
+  { id: "agachamento", title: "Agachamento para iniciantes", url: "guia-iniciante/videos/03-agachamento.mp4" },
+  { id: "flexao", title: "Flexão adaptada", url: "guia-iniciante/videos/04-flexao.mp4" },
+  { id: "prancha", title: "Prancha abdominal", url: "guia-iniciante/videos/05-prancha.mp4" },
+  { id: "recuperacao", title: "Recuperação e alongamento", url: "guia-iniciante/videos/06-recuperacao.mp4" }
+];
